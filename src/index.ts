98d1e4148c2182d3d0262575e4417b4cdb43ca5c
Tomas Lajda <tomas.lajda0@gmail.com>
2024-03-30 19:09:41 +0100

    connection to db, implementation of user schema

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('hello ts');
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log('Server running on http://localhost:8080/');
+});
+
+const mongoUrl =
+  'mongodb+srv://tomaslajda0:hnwG2pdGs8fTRBdQ@cluster0.xa8uh5d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.Promise = Promise;
+mongoose.connect(mongoUrl);
+mongoose.connection.on('error', (error: Error) => console.log(error));
